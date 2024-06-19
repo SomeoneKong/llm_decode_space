76dca41ffa0ab0cecd393b96de6d950cf6cd868e
someonekong <someonekong3001@gmail.com>
2024-06-19 11:18:14 +0800

    v2

--- gen_raw_graph.py
+++ gen_raw_graph.py
@@ -1,24 +1,39 @@
 import asyncio
 import math
-from typing import Optional
+from typing import Optional, List
+from pydantic import BaseModel
+from queue import PriorityQueue, Empty
+from threading import Lock
+from dataclasses import dataclass, field
 
 from llm_client import LlmClient, TokenProbInfo
-from pydantic import BaseModel
-from threading import Lock
 
 
 class GraphNodeModel(BaseModel):
     id: int = -1
     father_node_id: Optional[int]
     token_info: Optional[TokenProbInfo]
-    eos: bool = False
-    accum_prob: float
+    eos: bool
+    accum_logprob: float
+    seq_token_num: int
 
 
 class GraphNode:
     def __init__(self, model: GraphNodeModel):
         self.model = model
         self.father = None
+        self.children = []
+
+        self.total_trim_logprob = None
+        self.accum_total_trim_logprob = None
+
+    def trimmed_accum_logprob(self):
+        result = self.model.accum_logprob - (self.father.accum_total_trim_logprob if self.father is not None else 0)
+        assert result <= 0 + 1e-6, f'{result} {self.model.accum_logprob} {self.father.accum_total_trim_logprob if self.father is not None else 0}'
+        return result
+
+    def expand_rank_score(self):
+        return self.trimmed_accum_logprob()
 
     def get_all_prefix_token_ids(self):
         prefix = []
@@ -39,53 +54,87 @@ class GraphNode:
 
     def __str__(self):
         if self.model.token_info is None:
-            return f'{self.model.id} {self.model.accum_prob}'
-        return f'{self.model.id} {self.model.accum_prob:.6f} [{repr(self.father.get_all_prefix_text())}] {math.exp(self.model.token_info.logprob):.6f} {self.model.token_info.token_id} {repr(self.model.token_info.token)} {self.model.token_info.eos}'
+            return f'{self.model.id} {self.model.accum_logprob} {self.total_trim_logprob} {self.accum_total_trim_logprob}'
+        return f'{self.model.id} {self.model.accum_logprob:.6f} {self.total_trim_logprob} {self.accum_total_trim_logprob} [{repr(self.father.get_all_prefix_text())}] {math.exp(self.model.token_info.logprob):.6f} {self.model.token_info.token_id} {repr(self.model.token_info.token)} {self.model.token_info.eos}'
 
 
 class Graph:
+    @dataclass(order=True)
+    class PrioritizedItem:
+        priority: float
+        item: GraphNode = field(compare=False)
+
     def __init__(self):
-        self.nodes = dict()
         self.update_lock = Lock()
 
-        self.expand_queue = []
-        self.expand_queue_sorted = False
-        self.expand_queue_lock = Lock()
-
+        self.nodes = dict()
+        self.expand_queue = PriorityQueue()
         self.closed_node_list = []
 
-    def add_node(self, node_model: GraphNodeModel):
-        node = GraphNode(node_model)
-        if node_model.father_node_id is not None:
-            node.father = self.nodes[node_model.father_node_id]
-        if node_model.id == -1:
-            with self.update_lock:
-                node_model.id = len(self.nodes)
-                self.nodes[node_model.id] = node
-        else:
-            self.nodes[node_model.id] = node
+    def add_nodes(self,
+                  father_id: Optional[int],
+                  node_model_list: List[GraphNodeModel],
+                  node_list_to_expand: List[GraphNodeModel]
+                  ):
 
-        if not node.model.eos:
-            with self.expand_queue_lock:
-                self.expand_queue.append(node)
-                self.expand_queue_sorted = False
+        if father_id is not None:
+            father_node = self.nodes[father_id]
         else:
-            self.closed_node_list.append(node)
+            father_node = None
+
+        node_list = []
+        total_prob = 0
+        for node_model in node_model_list:
+            node = GraphNode(node_model)
+            node.father = father_node
+            if father_node is not None:
+                father_node.children.append(node)
+            node_list.append(node)
+
+            total_prob += math.exp(node_model.token_info.logprob if node_model.token_info is not None else 0)
+
+        expand_node_list = []
+        closed_node_list = []
+        for node in node_list:
+            if node.model.eos:
+                closed_node_list.append(node)
+            if node.model in node_list_to_expand and not node.model.eos:
+                expand_node_list.append(node)
+
+        with self.update_lock:
+            for node in node_list:
+                if node.model.id == -1:
+                    node.model.id = len(self.nodes)
+                self.nodes[node.model.id] = node
+
+            for node in closed_node_list:
+                self.closed_node_list.append(node)
+
+            if father_node is not None:
+                father_node.total_trim_logprob = math.log(total_prob)
+                father_node.accum_total_trim_logprob = math.log(total_prob)
+                if father_node.father is not None:
+                    father_node.accum_total_trim_logprob += father_node.father.accum_total_trim_logprob
+
+            for node in expand_node_list:
+                self.expand_queue.put(Graph.PrioritizedItem(priority= -node.expand_rank_score(), item=node))
+
+        # print(f'add_nodes: {len(node_list)} {len(expand_node_list)} {len(closed_node_list)}')
 
     def sort_and_get_next_expand_node(self):
-        with self.expand_queue_lock:
-            if not self.expand_queue_sorted:
-                self.expand_queue.sort(key=lambda x: x.model.accum_prob)
-                self.expand_queue_sorted = True
-            if len(self.expand_queue) == 0:
-                return None
-            return self.expand_queue.pop()
+        try:
+            item = self.expand_queue.get_nowait()
+        except Empty:
+            return None
+        return item.item
 
 
 async def fill_graph(llm_client,
                      message_list,
                      temperature=0.8,
                      top_p=0.95,
+
+                     sample_max_tokens=1000,
                      max_query_num=1000,
                      parallel_job_num=20,
                      ):
@@ -94,46 +143,70 @@ async def fill_graph(llm_client,
         id=0,
         father_node_id=None,
         token_info=None,
-        accum_prob=0,
+        eos=False,
+        accum_logprob=0,
+        seq_token_num=0,
     )
-    graph.add_node(start_node_model)
+    graph.add_nodes(None, [start_node_model], [start_node_model])
 
     query_task_list = []
 
-    async def expand_node(query_id, node):
+    async def expand_node(query_id, node, graph: Graph):
         query_prefix_token_ids = node.get_all_prefix_token_ids()
-        next_token_dist_result = await llm_client.calc_next_token_dist(
+        response_stream = llm_client.sample_trace_to_end_stream(
             message_list,
             query_prefix_token_ids,
+            prefix_logprob=node.model.accum_logprob,
             temperature=temperature,
-            top_p=top_p
+            top_p=top_p,
+            max_tokens=sample_max_tokens,
         )
 
-        result_nodes = []
-        for token_prob in next_token_dist_result['token_dist']:
-            new_node_model = GraphNodeModel(
-                father_node_id=node.model.id,
-                token_info=token_prob,
-                eos=token_prob.eos,
-                accum_prob=node.model.accum_prob + token_prob.logprob,
-            )
-            result_nodes.append(new_node_model)
+        current_node_model = node.model
 
-        return query_id, node, result_nodes
+        async for delta_info in response_stream:
+            finished_reason = delta_info['finished_reason']
+            token_dist = delta_info['token_dist']
+
+            if finished_reason is None:
+                delta_token_id = delta_info['delta_token_id']
+            else:
+                delta_token_id = None
+
+            next_node_model = None
+            result_nodes = []
+            expand_nodes = []
+            for token_prob in token_dist:
+                new_node_model = GraphNodeModel(
+                    father_node_id=current_node_model.id,
+                    token_info=token_prob,
+                    eos=token_prob.eos,
+                    accum_logprob=current_node_model.accum_logprob + token_prob.logprob,
+                    seq_token_num=current_node_model.seq_token_num + 1,
+                )
+                result_nodes.append(new_node_model)
+                if token_prob.token_id != delta_token_id:
+                    expand_nodes.append(new_node_model)
+                else:
+                    next_node_model = new_node_model
+
+            graph.add_nodes(current_node_model.id, result_nodes, expand_nodes)
+            # print(f'{len(graph.nodes)} {len(graph.expand_queue)} {len(graph.closed_node_list)} | {current_node_model.id} gen node: seq_len={current_node_model.seq_token_num} [{repr(graph.nodes[current_node_model.id].get_all_prefix_text())}] token_dist={token_dist}')
+
+            current_node_model = next_node_model
+            assert current_node_model is None or current_node_model.id != - 1
+            if current_node_model is None:
+                break
 
     query_num = 0
     while True:
         if len(query_task_list) == 0:
             done_task, pending_task = set(), set()
         else:
-            done_task, pending_task = await asyncio.wait(query_task_list)
+            done_task, pending_task = await asyncio.wait(query_task_list, timeout=3, return_when=asyncio.FIRST_COMPLETED)
 
         if len(done_task) > 0:
             for task in done_task:
-                query_id, job_node, result_nodes = task.result()
-                for node_model in result_nodes:
-                    graph.add_node(node_model)
-                    print(f'{query_id} {len(graph.expand_queue)} {len(graph.closed_node_list)} | {job_node.model.id} gen node: {node_model.id} {node_model.accum_prob:.6f} [{repr(graph.nodes[node_model.father_node_id].get_all_prefix_text())}] {node_model.token_info.token_id} {repr(node_model.token_info.token)} {node_model.token_info.eos}')
                 query_task_list.remove(task)
 
         query_task_list = list(pending_task)
@@ -142,15 +215,19 @@ async def fill_graph(llm_client,
             node = graph.sort_and_get_next_expand_node()
             if node is None:
                 break
-            query_task_list.append(asyncio.create_task(expand_node(query_num, node)))
+            query_task_list.append(asyncio.create_task(expand_node(query_num, node, graph)))
             query_num += 1
+            print(f'create query {query_num} query_task_list={len(query_task_list)} {len(graph.nodes)} {graph.expand_queue.qsize()}, {node.expand_rank_score():.6f} seq_len={node.model.seq_token_num} [{repr(graph.nodes[node.model.father_node_id].get_all_prefix_text()) if node.model.father_node_id is not None else ""}] {repr(node.model.token_info.token) if node.model.token_info is not None else ""}')
+
+            # 每次只添加一个，然后等下一轮（3s）
+            break
 
         if len(query_task_list) == 0:
             break
 
-        await asyncio.sleep(0.05)
+        print(f'nodes={len(graph.nodes)} queue={graph.expand_queue.qsize()} closed={len(graph.closed_node_list)} running={len(query_task_list)}')
 
-    print(f'query_num: {query_num} {len(graph.nodes)} {len(graph.expand_queue)}')
+    print(f'query_num: {query_num} {len(graph.nodes)} {graph.expand_queue.qsize()}')
 
     return graph
 
@@ -164,6 +241,8 @@ B. 两直线被第三条直线所截，同位角相等
 C. 如果a、b都是实数，那么a+b=b+a
 
 D. 抛掷1个均匀的骰子，出现6点朝上
+
+请直接给出答案，不要解释，不要附带任何其他文本。
 '''.strip()
 
 
@@ -175,12 +254,24 @@ def main():
         {"role": "user", "content": prompt},
     ]
     graph = asyncio.run(
-        fill_graph(client, message_list, temperature=0.8, top_p=0.9, max_query_num=20000)
+        fill_graph(client, message_list, temperature=0.8, top_p=0.9, max_query_num=300)
     )
 
     for node in graph.nodes.values():
         # print(node)
         pass
+
+    graph.closed_node_list.sort(key=lambda x: x.trimmed_accum_logprob(), reverse=True)
+    for node in graph.closed_node_list:
+        print(f'{node.trimmed_accum_logprob():.6f} {node.model.seq_token_num} [{repr(node.get_all_prefix_text())}]')
+
+    # 为了优化数值稳定性，采用从小加到大的方式计算
+    finished_prob = 0
+    graph.closed_node_list.sort(key=lambda x: x.trimmed_accum_logprob())
+    for node in graph.closed_node_list:
+        finished_prob += math.exp(node.trimmed_accum_logprob())
+
+    print(f'finished_prob: {finished_prob}')
 
 
 if __name__ == "__main__":
--- llm_client.py
+++ llm_client.py
@@ -33,7 +33,7 @@ class LlmClient:
             if accum_prob >= top_p:
                 break
         if accum_prob < top_p:
-            print(f'warning: accum_prob < top_p, accum_prob={accum_prob}, top_p={top_p}')
+            print(f'warning: accum_prob < top_p, token_num={len(token_dist)} accum_prob={accum_prob}, top_p={top_p}')
             return token_dist
         return token_dist[:i + 1]
 
@@ -55,7 +55,7 @@ class LlmClient:
                     },
                     max_tokens=1,
                     logprobs=True,
-                    top_logprobs=20,
+                    top_logprobs=100,
                 )
                 break
             except Exception as e:
@@ -80,17 +80,140 @@ class LlmClient:
             'all_token_dist': token_dist,
         }
 
+    async def sample_trace_stream(self,
+                                  message_list,
+                                  prefix: List[int],
+                                  prefix_logprob: float = 0,
+                                  top_p: float = 0.9,
+                                  temperature: float = 0.8,
+                                  max_tokens=1000,
+                                  ):
+        response = await self.client.chat.completions.create(
+            model=self.model_name,
+            messages=message_list,
+            temperature=temperature,
+            extra_body={
+                "force_answer_prefix_token_ids": prefix,
+                "output_log_prob_token_id": True,
+            },
+            max_tokens=max_tokens,
+            logprobs=True,
+            top_logprobs=100,
+            stream=True,
+        )
+
+        accum_prefix = prefix[:]
+        accum_logprob = prefix_logprob
+        async for message in response:
+            choice0 = message.choices[0]
+            if choice0.delta.content is None:
+                continue
+
+            delta_token = choice0.delta.content
+            logprobs_info = choice0.logprobs.content[0]
+            delta_token_id = logprobs_info.token_id
+            delta_token_logprob = logprobs_info.logprob
+            top_token_dist = logprobs_info.top_logprobs
+
+            token_dist = [TokenProbInfo(
+                token_id=token_prob.token_id,
+                token=token_prob.token,
+                logprob=token_prob.logprob,
+                eos=token_prob.eos,
+                bytes=token_prob.bytes,
+            ) for token_prob in top_token_dist]
+            trimmed_token_dist = self.trim_prob_dist(token_dist, top_p)
+
+            delta_info = {
+                'prefix': accum_prefix[:],
+                'prefix_logprob': accum_logprob,
+
+                'token_dist': trimmed_token_dist,
+                'all_token_dist': token_dist,
+
+                'delta_token': delta_token,
+                'delta_token_id': delta_token_id,
+                'delta_token_logprob': delta_token_logprob,
+                'finished_reason': choice0.finish_reason,
+            }
+
+            out_of_top_p = delta_token_id not in [token_prob.token_id for token_prob in trimmed_token_dist]
+            if out_of_top_p:
+                delta_info['finished_reason'] = 'out_of_top_p'
+
+            yield delta_info
+
+            if out_of_top_p:
+                break
+
+            accum_prefix.append(delta_token_id)
+            accum_logprob += delta_token_logprob
+
+        await response.close()
+
+    async def sample_trace_to_end_stream(
+            self,
+            message_list,
+            prefix: List[int],
+            prefix_logprob: float = 0,
+            top_p: float = 0.9,
+            temperature: float = 0.8,
+            max_tokens=1000,
+            ):
+        while True:
+            delta_info = None
+            async for delta_info in self.sample_trace_stream(
+                message_list,
+                prefix,
+                prefix_logprob,
+                top_p,
+                temperature,
+                max_tokens,
+            ):
+                if delta_info['finished_reason'] == 'out_of_top_p':
+                    delta_token_logprob = delta_info['delta_token_logprob']
+                    pre_sum_prob = sum([math.exp(token_prob.logprob) for token_prob in delta_info['token_dist'] if token_prob.logprob > delta_token_logprob])
+                    # print(f'drop out_of_top_p: {repr(delta_info["delta_token"])} {delta_info["delta_token_id"]} {delta_info["delta_token_logprob"]:.6f} {pre_sum_prob}')
+                    break
+                yield delta_info
+
+            if delta_info['finished_reason'] == 'stop':
+                break
+
+            # print(f'continue from {delta_info["finished_reason"]} ...')
+            prefix = delta_info['prefix']
+            prefix_logprob = delta_info['prefix_logprob']
+
+
+
 
 if __name__ == "__main__":
     import asyncio
 
     client = LlmClient()
+
+    prompt = '''
+下列事件中，属于必然事件的是__
+
+A. 任意数的绝对值都是正数
+
+B. 两直线被第三条直线所截，同位角相等
+
+C. 如果a、b都是实数，那么a+b=b+a
+
+D. 抛掷1个均匀的骰子，出现6点朝上
+'''.strip()
+
     message_list = [
-        {"role": "user", "content": "你好"},
+        {"role": "user", "content": prompt},
     ]
     prefix = [
     ]
 
+    async def test_main():
+        async for delta in client.sample_trace_to_end_stream(message_list, prefix, max_tokens=1000):
+            print(f'prefix={delta["prefix"][-5:]}, delta={delta["delta_token_id"]}, {repr(delta["delta_token"])}, {delta["delta_token_logprob"]:.6f}, {delta["finished_reason"]}, {delta["token_dist"]}')
+
     asyncio.run(
-        client.calc_next_token_dist(message_list, prefix)
+        test_main()
     )
